1e2d6535a718df38d7421062c936f8dafee24229
Pedro Bustamante <pedrobustamanted@gmail.com>
2023-12-16 22:42:03 +0100

    Gitsigns add

--- lua/plugins/fugitive.lua
+++ lua/plugins/fugitive.lua
@@ -3,6 +3,7 @@ return {
     -- Sirve para que el plugin no se cargue hasta que no lo necesite (use git)
     cmd = {"G", "Git"},
     keys = {
+        -- <cr> es el return
         {"<leader>g",":G<cr>", desc = "Abre fugitive"},
         {"<leader>gp", ":Git pull<cr>", desc = "Git pull"}
     }
---
+++ lua/plugins/gitsigns.lua
@@ -0,0 +1,8 @@
+return {
+    "lewis6991/gitsigns.nvim",
+    config = true,
+    opts = {
+        numhl = true,
+        maz_file_length = 10000,
+    }
+}
